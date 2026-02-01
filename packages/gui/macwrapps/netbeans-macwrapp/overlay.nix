6feb0662e3938a388f7e501af5c24ef21a2f9100
Juanma <juanma151@gmail.com>
2026-02-01 18:33:45 +0100

    bug

--- packages/gui/macwrapps/netbeans-macwrapp/overlay.nix
+++ packages/gui/macwrapps/netbeans-macwrapp/overlay.nix
@@ -3,7 +3,7 @@
 final: prev: {
   netbeans-macwrapp = import ./. {
     system = prev.stdenv.hostPlatform.system;
-    stdenvNoCC = prev.stdenvNoCC =;
+    stdenvNoCC = prev.stdenvNoCC;
     netbeans = prev.netbeans;
     mkiconapple = prev.mkiconapple;
     jdkPkg = prev.openjdk25;
